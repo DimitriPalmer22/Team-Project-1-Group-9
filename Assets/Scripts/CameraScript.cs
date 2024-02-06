13bcd89f0b5a1b676063e25a8ad39efff94406f1
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-02-06 15:27:10 -0500

    Camera transitions

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -10,15 +10,15 @@ public enum CameraMode
 {
     // Follow the player
     Follow,
-        
+
     // Stay static at a fixed point
-    Fixed 
+    Fixed
 }
 
 public class CameraScript : MonoBehaviour
 {
     private const float FixedPointTransitionTime = .5f;
-    
+
     /// <summary>
     /// A single instance of this script that represents the only camera script in the current scene 
     /// </summary>
@@ -30,7 +30,7 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private CameraMode cameraMode = CameraMode.Follow;
 
     public CameraMode CameraMode => cameraMode;
-    
+
     /// <summary>
     /// The transform to follow when the camera is in "Follow" mode 
     /// </summary>
@@ -46,6 +46,10 @@ public class CameraScript : MonoBehaviour
     /// </summary>
     [SerializeField] private float cameraY;
 
+    /// <summary>
+    /// Variable to keep track of if the camera is currently moving from one mode to another.
+    /// </summary>
+    private bool _transitioningPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -63,11 +67,15 @@ public class CameraScript : MonoBehaviour
         switch (cameraMode)
         {
             case CameraMode.Follow:
-                FollowTarget();
+                if (!_transitioningPosition)
+                    FollowTarget();
                 break;
+    
             case CameraMode.Fixed:
-                SetPositionToFixedPoint();
+                if (!_transitioningPosition)
+                    SetPositionToFixedPoint();
                 break;
+            
             default:
                 Debug.LogError($"\"{cameraMode}\" CASE IS NOT HANDLED IN THE CAMERA SCRIPT!");
                 break;
@@ -82,7 +90,7 @@ public class CameraScript : MonoBehaviour
         // If there is no target object, skip
         if (targetObject == null)
             return;
-        
+
         var oldPosition = transform.position;
 
         // Move the camera's y-value so that it always stays level
@@ -97,14 +105,14 @@ public class CameraScript : MonoBehaviour
         // If there is no fixed point, skip
         if (fixedPoint == null)
             return;
-        
+
         var oldPosition = transform.position;
         var fixedPointPosition = fixedPoint.position;
 
         // Move the camera's position to the fixed point's position
-        transform.position = new Vector3(fixedPointPosition.x, fixedPointPosition.y, oldPosition.z);
+        transform.position = new Vector3(fixedPointPosition.x, cameraY, oldPosition.z);
     }
-    
+
     /// <summary>
     /// Change the current camera mode
     /// </summary>
@@ -118,8 +126,10 @@ public class CameraScript : MonoBehaviour
         switch (mode)
         {
             case CameraMode.Follow:
+                StartCoroutine(TransitionToTarget());
                 break;
             case CameraMode.Fixed:
+                StartCoroutine(TransitionToFixedPoint());
                 break;
             default:
                 Debug.LogError($"\"{mode}\" CASE IS NOT HANDLED IN THE CAMERA SCRIPT!");
@@ -139,16 +149,53 @@ public class CameraScript : MonoBehaviour
         ChangeCameraMode(CameraMode.Fixed);
     }
 
+    /// <summary>
+    /// Coroutine to smoothly transition the camera's position to the fixed point
+    /// </summary>
     IEnumerator TransitionToFixedPoint()
     {
-        float updatePrecision = .1f;
+        Vector2 startPosition = transform.position;
+
+        float updatePrecision = Time.deltaTime;
+        _transitioningPosition = true;
         
         for (float i = 0; i < FixedPointTransitionTime; i += updatePrecision)
         {
-            Debug.Log($"I: {i}");
+            var oldCameraPosition = transform.position;
+            
+            // Lerp the points to determine where the camera should be right now
+            var lerpedPosition = Vector2.Lerp(startPosition, fixedPoint.position, i / FixedPointTransitionTime);
+            
+            transform.position = new Vector3(lerpedPosition.x, oldCameraPosition.y, oldCameraPosition.z);
+            
             yield return new WaitForSeconds(updatePrecision);
         }
+
+        _transitioningPosition = false;
     }
-    
-    
+
+    /// <summary>
+    /// Coroutine to smoothly transition the camera's position to the target
+    /// </summary>
+    IEnumerator TransitionToTarget()
+    {
+        Vector2 startPosition = transform.position;
+
+        float updatePrecision = Time.deltaTime;
+        _transitioningPosition = true;
+        
+        for (float i = 0; i < FixedPointTransitionTime; i += updatePrecision)
+        {
+            var oldCameraPosition = transform.position;
+            
+            // Lerp the points to determine where the camera should be right now
+            var lerpedPosition = Vector2.Lerp(startPosition, targetObject.position, i / FixedPointTransitionTime);
+            
+            transform.position = new Vector3(lerpedPosition.x, oldCameraPosition.y, oldCameraPosition.z);
+            
+            yield return new WaitForSeconds(updatePrecision);
+        }
+
+        _transitioningPosition = false;
+    }
 }
